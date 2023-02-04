ad3db760438562a394be8d7cd35c8b4195a292fe
Magistu <77356543+Magistu@users.noreply.github.com>
2023-02-04 17:23:15 +0300

    networking

--- common/src/main/java/ru/itzme1on/siegemachines/entity/projectile/projectiles/GiantArrow.java
+++ common/src/main/java/ru/itzme1on/siegemachines/entity/projectile/projectiles/GiantArrow.java
@@ -1,15 +1,22 @@
 package ru.itzme1on.siegemachines.entity.projectile.projectiles;
 
+import com.google.common.base.Suppliers;
 import net.minecraft.client.util.math.Vector3d;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import ru.itzme1on.siegemachines.registry.ItemRegistry;
 
+import java.util.function.Supplier;
+
 public class GiantArrow extends PersistentProjectileEntity {
+    public static final Supplier<EntityType<GiantArrow>> TYPE = Suppliers.memoize(() -> EntityType.Builder.create((EntityType.EntityFactory<GiantArrow>) GiantArrow::new, SpawnGroup.MISC).setDimensions(0.98F, 0.7F).maxTrackingRange(8).build("giant_arrow"));
+    
     public GiantArrow(EntityType<GiantArrow> entityType, World world) {
         super(entityType, world);
     }
--- common/src/main/java/ru/itzme1on/siegemachines/registry/EntityRegistry.java
+++ common/src/main/java/ru/itzme1on/siegemachines/registry/EntityRegistry.java
@@ -10,7 +10,7 @@ import ru.itzme1on.siegemachines.entity.projectile.projectiles.GiantArrow;
 public class EntityRegistry {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(SiegeMachines.MOD_ID, Registry.ENTITY_TYPE_KEY);
 
-//    public static final RegistrySupplier<EntityType<GiantArrow>> GIANT_ARROW = ENTITIES.register("giant_arrow", );
+    public static final RegistrySupplier<EntityType<GiantArrow>> GIANT_ARROW = ENTITIES.register("giant_arrow", GiantArrow.TYPE);
 
     public static void init() {
         ENTITIES.register();
