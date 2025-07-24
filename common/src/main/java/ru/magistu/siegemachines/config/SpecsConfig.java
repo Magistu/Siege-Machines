6e489f832128244368a8d9862bea98cc8abf3934
Magistu <77356543+magistu@users.noreply.github.com>
2025-07-24 17:50:38 +0300

    fixed broken siege-machines config

--- common/src/main/java/ru/magistu/siegemachines/config/SpecsConfig.java
+++ common/src/main/java/ru/magistu/siegemachines/config/SpecsConfig.java
@@ -6,9 +6,9 @@ public final class SpecsConfig {
     public static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
     public static final ModConfigSpec SPEC;
 
-    public static final ModConfigSpec.ConfigValue<Float> FIRE_DAMAGE_MULTIPLIER = BUILDER.define("fireDamageMultiplier", 3.0f);
-    public static final ModConfigSpec.ConfigValue<Float> EXPLOSION_DAMAGE_MULTIPLIER = BUILDER.define("explosionDamageMultiplier", 2.0f);
-    public static final ModConfigSpec.ConfigValue<Float> ARROW_DAMAGE_MULTIPLIER = BUILDER.define("arrowDamageMultiplier", 0.5f);
+    public static final ModConfigSpec.ConfigValue<Float> FIRE_DAMAGE_MULTIPLIER;
+    public static final ModConfigSpec.ConfigValue<Float> EXPLOSION_DAMAGE_MULTIPLIER;
+    public static final ModConfigSpec.ConfigValue<Float> ARROW_DAMAGE_MULTIPLIER;
 
     public static final SiegeMachineSpecs MORTAR;
     public static final SiegeMachineSpecs CULVERIN;
@@ -24,6 +24,10 @@ public final class SpecsConfig {
 
     static {
         BUILDER.push("siege_machines");
+
+        FIRE_DAMAGE_MULTIPLIER = BUILDER.define("fireDamageMultiplier", 3.0f);
+        EXPLOSION_DAMAGE_MULTIPLIER = BUILDER.define("explosionDamageMultiplier", 2.0f);
+        ARROW_DAMAGE_MULTIPLIER = BUILDER.define("arrowDamageMultiplier", 0.5f);
 
         MORTAR = new SiegeMachineSpecs(BUILDER, "mortar", 80, 200, 2.5f, 0.2f, 1.5f);
         CULVERIN = new SiegeMachineSpecs(BUILDER, "culverin", 150, 260, 3.5f, 0.03f, 3.0f);
