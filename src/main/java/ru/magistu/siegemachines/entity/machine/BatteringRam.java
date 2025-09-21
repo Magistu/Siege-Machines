50cce485d4c229466c4e6560bb5977173b888389
Magistu <77356543+magistu@users.noreply.github.com>
2025-09-21 23:47:18 +0300

    fixed bugs

--- src/main/java/ru/magistu/siegemachines/entity/machine/BatteringRam.java
+++ src/main/java/ru/magistu/siegemachines/entity/machine/BatteringRam.java
@@ -154,7 +154,7 @@ public class BatteringRam extends Machine implements MachineGeoEntity, Reloading
             Entity source = this.lastUsedEntity == null ? this : this.lastUsedEntity;
             MissileExplosion explosion = new MissileExplosion(this.level(), source, this.level().damageSources().explosion(this, source), getExplosionDamageCalculator(), x, y, z, 2, false, Explosion.BlockInteraction.DESTROY);
             explosion.explode();
-            explosion.finalizeExplosion(true);
+            explosion.finalizeExplosion(false);
         }
     }
 
--- src/main/java/ru/magistu/siegemachines/entity/machine/Machine.java
+++ src/main/java/ru/magistu/siegemachines/entity/machine/Machine.java
@@ -597,7 +597,6 @@ public abstract class Machine extends Mob implements MenuProvider, Useable {
     }
 
     public double getExplosionDamageMultiplier() {
-        System.out.println(this.type.specs.explosiondamagemultiplier.get());
         return this.type.specs.explosiondamagemultiplier.get();
     }
 
